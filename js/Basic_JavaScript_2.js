e91362fba9cbb93dd21608647d7f8a0ee64ba51f
Hayden Stark <haydenstark@aol.com>
2020-08-01 17:40:45 -0500

    Add Basic JS 2

---
+++ js/Basic_JavaScript_2.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var a = document.getElementById("testID");
+    var b="blue";
+    a.style.color=b;
+}
+
+
+function secondFunction() {
+    var sent = "This is";
+    sent += " A SENTENCE.";
+    document.getElementById("string").innerHTML = sent;
+}
