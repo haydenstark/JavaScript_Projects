44758c3ccfefd5d551724c31fcab26c31a6d794c
Hayden Stark <haydenstark@aol.com>
2020-08-01 16:37:22 -0500

    Add first assignment

---
+++ js/Javascript_file.js
@@ -0,0 +1,16 @@
+window.alert("Hello, World!");          //This will open a window displaying "Hellow, World!"
+
+document.write("Hello, again!");        //This will display "Hello, again!" on the page
+
+var a = "This is what";                 //Assigning a string to variable a
+var b = " concatenating looks like!";   //Assigning a string to variable b
+function concatenate(x, y) {            //Defining a function to add two variables together
+    return c = x+y                      //Returning variable c which holds the result of the function
+}
+alert(concatenate(a, b));               //This displays variable a and b together in an alert window
+
+document.write(6+6);                    //This will display 12 on the page
+
+var redText="This text is red.";        //Assigning a string to variable redText
+var result= redText.fontcolor("red");   //Making the previous variable have red font
+document.write(result);                 //This will display "This text is red" in red font on the page
